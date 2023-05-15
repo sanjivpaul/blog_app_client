693d310a71ce487a257de20677e73d1ba80af95c
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-05-15 20:15:23 +0530

    delete functionality created

--- src/Components/SinglePost/SinglePost.jsx
+++ src/Components/SinglePost/SinglePost.jsx
@@ -1,16 +1,17 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Context } from "../../Context/context";
 
 export default function SinglePost() {
   const location = useLocation();
   // console.log(location.pathname.split("/")[2]);
   // path will return the id of the post and we can fetch the post by using _id
   const path = location.pathname.split("/")[2];
-
   const [post, setPost] = useState({});
   const publicFolder = "http://localhost:5000/images/";
+  const { user } = useContext(Context);
 
   useEffect(() => {
     const getPost = async () => {
@@ -21,6 +22,18 @@ export default function SinglePost() {
     getPost();
   }, [path]);
   // when ever path is changes fire this useEffect
+
+  // delete function
+  const handleDelete = async () => {
+    try {
+      await axios.delete("http://localhost:5000/api/posts/" + path, {
+        data: {
+          username: user.username,
+        },
+      });
+      window.location.replace("/");
+    } catch (error) {}
+  };
   return (
     <>
       <div className="singlePost">
@@ -36,10 +49,13 @@ export default function SinglePost() {
           <h1 className="singlePostTitle">
             {post.title}
             {/* if post username is same as login username then edit options are visible */}
-            {post.username === userDelete.username && (
+            {post.username === user?.username && (
               <div className="singlePostEdit">
                 <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
-                <i className="singlePostIcon fa-regular fa-trash-can"></i>
+                <i
+                  className="singlePostIcon fa-regular fa-trash-can"
+                  onClick={handleDelete}
+                ></i>
               </div>
             )}
           </h1>
