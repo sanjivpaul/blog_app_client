25ddf9c49362f58d965210b4db4b79a49c8bd3be
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-05-18 23:10:37 +0530

    singlepost edit functionality created

--- src/Components/SinglePost/SinglePost.jsx
+++ src/Components/SinglePost/SinglePost.jsx
@@ -12,12 +12,18 @@ export default function SinglePost() {
   const [post, setPost] = useState({});
   const publicFolder = "http://localhost:5000/images/";
   const { user } = useContext(Context);
+  // this states are for edit posts
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get("http://localhost:5000/api/posts/" + path);
       // console.log(res.data.post);
       setPost(res.data.post);
+      setTitle(res.data.post.title);
+      setDesc(res.data.post.desc);
     };
     getPost();
   }, [path]);
@@ -34,6 +40,18 @@ export default function SinglePost() {
       window.location.replace("/");
     } catch (error) {}
   };
+
+  const handleUpdate = async () => {
+    try {
+      await axios.put("http://localhost:5000/api/posts/" + path, {
+        username: user.username,
+        title,
+        desc,
+      });
+      // window.location.reload();
+      setEditMode(false);
+    } catch (error) {}
+  };
   return (
     <>
       <div className="singlePost">
@@ -46,19 +64,37 @@ export default function SinglePost() {
               className="singlePostImg"
             />
           )}
-          <h1 className="singlePostTitle">
-            {post.title}
-            {/* if post username is same as login username then edit options are visible */}
-            {post.username === user?.username && (
-              <div className="singlePostEdit">
-                <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
-                <i
-                  className="singlePostIcon fa-regular fa-trash-can"
-                  onClick={handleDelete}
-                ></i>
-              </div>
-            )}
-          </h1>
+
+          {
+            // if its edit mode take input
+            editMode ? (
+              <input
+                type="text"
+                value={title}
+                className="singlePostTitleEditModeInput"
+                onChange={(e) => setTitle(e.target.value)}
+                // autoFocus
+              />
+            ) : (
+              // if its not update mode write this h1 text
+              <h1 className="singlePostTitle">
+                {title}
+                {/* if post username is same as login username then edit options are visible */}
+                {post.username === user?.username && (
+                  <div className="singlePostEdit">
+                    <i
+                      className="singlePostIcon fa-regular fa-pen-to-square"
+                      onClick={() => setEditMode(true)}
+                    ></i>
+                    <i
+                      className="singlePostIcon fa-regular fa-trash-can"
+                      onClick={handleDelete}
+                    ></i>
+                  </div>
+                )}
+              </h1>
+            )
+          }
           <div className="singlePostInfo">
             <span className="singlePostAuthor">
               Author:
@@ -70,7 +106,22 @@ export default function SinglePost() {
               {new Date(post.createdAt).toDateString()}
             </span>
           </div>
-          <p className="singlePostDesc">{post.desc}</p>
+          {editMode ? (
+            <textarea
+              className="singlePostDescEditModeInput"
+              value={desc}
+              // autoFocus
+              onChange={(e) => setDesc(e.target.value)}
+            />
+          ) : (
+            <p className="singlePostDesc">{desc}</p>
+          )}
+          {/* if edit mode then show update button */}
+          {editMode && (
+            <button className="singlePostButton" onClick={handleUpdate}>
+              Update
+            </button>
+          )}
         </div>
       </div>
     </>
